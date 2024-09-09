b94f61dd6f30bd2b588f7f0e0568044ebd86ecc0
yi <73318855+Shatterdest@users.noreply.github.com>
2024-09-08 21:59:44 -0400

    #1, setup

--- explortle/src/views/HomeView.vue
+++ explortle/src/views/HomeView.vue
@@ -1,9 +1,9 @@
 <script setup lang="ts">
-import TheWelcome from '../components/TheWelcome.vue'
 </script>
 
 <template>
+  <header>Explortle</header>
   <main>
-    <TheWelcome />
+    <h1>Hello World!</h1>
   </main>
 </template>
