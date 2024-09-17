ec559918ab9ba01601d4056291c13e2a33fe76f8
yi <73318855+Shatterdest@users.noreply.github.com>
2024-09-17 16:39:40 -0400

    Revert "Merge branch 'dev' of https://github.com/Shatterdest/Explortle-web into dev"
    This reverts commit 5a62f00cbe314a264ac0d647b4601795e62e5f1d, reversing
    changes made to 0919e5d67b970148f262271fa28f3594c9a5ff48.

--- explortle/src/router/index.ts
+++ explortle/src/router/index.ts
@@ -8,7 +8,7 @@ const router = createRouter({
     {
       path: '/',
       name: 'home',
-      component: AboutView
+      component: HomeView
     },
     {
       path: '/contact',
---
+++ explortle/src/views/HomeView.vue
@@ -0,0 +1,27 @@
+<script setup lang="ts">
+import { useRouteStore } from '@/stores/route'
+import { onMounted } from 'vue'
+
+onMounted(() => {
+  const route = useRouteStore()
+  route.$reset()
+  route.route[0].current = true
+})
+</script>
+
+<template>
+  <main>
+    <h3>
+      Explortle is a student-led initiative that aims to bring experienced technical career
+      professionals to conduct hands-on workshops in schools across New York City. Through these
+      workshops, our goal is to bridge the gap between students and career professionals from
+      diverse industries. These hands-on workshops cover a broad range of topics, from STEM, to
+      finance, and beyond. Our mission is to provide underrepresented students with interactive,
+      real-world learning experiences that inspire curiosity and foster a passion for exploring
+      different career paths. We prioritize bringing in engaging speakers, immersive workshops, and
+      lessons that make students excited to explore different fields.
+    </h3>
+    <h2>Associates</h2>
+    <h3>ACE and AIASINY</h3>
+  </main>
+</template>
