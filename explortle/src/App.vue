921c7a29db84bd0eaf4685985210faffc6016c84
yi <73318855+Shatterdest@users.noreply.github.com>
2024-10-29 22:37:44 -0400

    instagram

--- explortle/src/App.vue
+++ explortle/src/App.vue
@@ -1,17 +1,33 @@
 <script setup lang="ts">
 import { RouterLink, RouterView } from 'vue-router'
-import NavBar from '@/components/NavBar.vue';
+import NavBar from '@/components/NavBar.vue'
 </script>
 
 <template>
   <NavBar> </NavBar>
   <main class="main">
-  <RouterView /></main>
-  <footer>Follow our socials!</footer>
+    <RouterView />
+  </main>
+  <footer class="social-wrapper">
+    Follow our socials!
+    <div>
+      <a href="https://www.instagram.com/explortle/" target="_blank">
+        <img class='icon' src="../src/assets/imgs/insta.png" alt=""
+      /></a>
+    </div>
+  </footer>
 </template>
 
 <style scoped>
 .main {
   background-color: #ffffff;
+}
+
+.icon {
+  width: 50px;
+}
+
+.social-wrapper {
+  display:inline;
 }
 </style>
