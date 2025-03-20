89b74c3c316dc3e937280525ad4cf6c168f4abac
yi <73318855+Shatterdest@users.noreply.github.com>
2025-03-19 22:29:37 -0400

    nitpick router change

--- explortle/src/stores/route.ts
+++ explortle/src/stores/route.ts
@@ -1,18 +1,18 @@
-import { ref, computed } from 'vue'
+import { ref } from 'vue'
 import { defineStore } from 'pinia'
 
 export const useRouteStore = defineStore('route', () => {
   const route = ref([
     { name: 'About Us', href: '/', current: true },
-    { name: 'Contact Us', href: '/contact', current: false },
     { name: 'Our Mission', href: '/whatwedo', current: false },
+    { name: 'Contact Us', href: '/contact', current: false },
     { name: 'Apply', href: '/apply', current: false },
   ])
   function $reset() {
     route.value = [
       { name: 'About Us', href: '/', current: false },
-      { name: 'Contact Us', href: '/contact', current: false },
       { name: 'Our Mission', href: '/whatwedo', current: false },
+      { name: 'Contact Us', href: '/contact', current: false },
       { name: 'Apply', href: '/apply', current: false },
     ]
   }
