0f3567d74b3ac0133838929600169fac3cea2413
Yi Xia <73318855+Shatterdest@users.noreply.github.com>
2025-04-20 16:55:07 -0400

    f

--- explortle/cms/tailwind.config.js
+++ explortle/cms/tailwind.config.js
@@ -1,10 +1,13 @@
-module.exports = {
-  content: [
-    './index.html',
-    './src/**/*.{vue,js,ts,jsx,tsx}',
-  ],
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: ['./index.html', './src/**/*.{vue,js,ts,jsx,tsx}'],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        heading: ['Poppins', 'sans-serif'],
+        body: ['Inter', 'sans-serif'],
+      }
+    }
   },
-  plugins: [],
+  plugins: []
 }
