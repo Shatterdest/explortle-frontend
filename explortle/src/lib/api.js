61097b219fccd21767ed8a442028f41aebe01b27
Yi Xia <73318855+Shatterdest@users.noreply.github.com>
2025-04-20 22:56:36 -0400

    f

--- explortle/src/lib/api.js
+++ explortle/src/lib/api.js
@@ -1,14 +1,24 @@
 import axios from 'axios'
 
+const FRONTEND_API_KEY = import.meta.env.VITE_FRONTEND_API_KEY;
+
 const api = axios.create({
   baseURL: import.meta.env.VITE_BACKEND_URL, 
 })
 
-// Attach JWT if available
 api.interceptors.request.use(config => {
-  const token = localStorage.getItem('token')
-  if (token) config.headers.Authorization = `Bearer ${token}`
-  return config
-})
+  const token = localStorage.getItem('token');
+  if (token) {
+    config.headers.Authorization = `Bearer ${token}`;
+  }
+
+  if (FRONTEND_API_KEY) {
+    config.headers['x-api-key'] = FRONTEND_API_KEY;
+  }
+
+  return config;
+}, error => {
+  return Promise.reject(error);
+});
 
 export default api
